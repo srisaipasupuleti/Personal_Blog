697918b460aff31ab06ca3e1267f1159cd45bdf4
Srisai Pasupuleti <srisaichiyan24@gmail.com>
2024-11-28 02:14:12 +0530

    Integrated tailwind to the project.

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -117,14 +117,10 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-# URL prefix for static files
-STATIC_URL = '/static/'
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 
 # For serving static files in production (use with collectstatic)
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles") # Collected static files location
